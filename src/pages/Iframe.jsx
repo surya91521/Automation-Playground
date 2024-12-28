bd5bdc97677e17a4ddf4e419e72ed40e63c0ee55
surya91521 <suryanarayanan.s@browserstack.com>
2024-12-28 10:26:36 +0530

    iframe modified

--- src/pages/Iframe.jsx
+++ src/pages/Iframe.jsx
@@ -5,7 +5,7 @@ function Iframe() {
     <div style={{ textAlign: "center", padding: "20px" }}>
       <iframe
         title="input-frame"
-        style={{ width: "100%", height: "300px", border: "2px solid black" }}
+        style={{ width: "100%", height: "200px", border: "2px solid black" }}
         srcDoc={`
           <!DOCTYPE html>
           <html lang="en">
@@ -25,22 +25,22 @@ function Iframe() {
       <div style={{ marginTop: "20px" }}>
         <iframe
           title="outer-frame"
-          style={{ width: "800px", height: "800px", border: "2px solid black" }}
+          style={{ width: "800px", height: "500px", border: "2px solid black" }}
           srcDoc={`
             <!DOCTYPE html>
             <html lang="en">
               <head>
                 <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="viewport" content="width=800px, initial-scale=1.0" />
                 <title>Iframe Content</title>
               </head>
-              <body style="text-align: center; padding: 20px; width: '100%'">
+              <body style="text-align: center; width: '800px'">
                 <p>The below element is present inside nested iframe.</p>
-              <iframe
-                title="inner-frame"
-                style={{ width: "100%", height: "900px" }}
-                src="https://en.wikipedia.org/wiki/Anime"
-              ></iframe>
+                <iframe
+                  title="inner-frame"
+                  style={{ width: '800px', height: '400px' }} 
+                  src="https://hianime.to/home" 
+                ></iframe>
               </body>
             </html>
           `}
